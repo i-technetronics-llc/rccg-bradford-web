46c5fd65b9f513b020c98e702837b1f6ceba0fd2
Quadds <akinpelumoyo@gmail.com>
2024-07-21 23:12:53 +0100

    corrections

--- src/pages/about_us.tsx
+++ src/pages/about_us.tsx
@@ -8,14 +8,18 @@ import "react-multi-carousel/lib/styles.css";
 import AboutUsView from "@/components/AboutUsView";
 
 export default function AboutUs() {
+  // const [isHovered, setIsHovered] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
   const img = [
+  const img = [
     "https://www.rccgbradford.org.uk/wp-content/uploads/2019/05/rccg3-1.jpg",
+    "https://www.rccgbradford.org.uk/wp-content/uploads/2019/05/rccg8.jpg",
     "https://www.rccgbradford.org.uk/wp-content/uploads/2019/05/rccg8.jpg",
   ];
   const responsive = {
     superLargeDesktop: {
+      // the naming can be any, depends on you.
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 1,
@@ -39,6 +43,12 @@ export default function AboutUs() {
   // "To have a member of RCCG in every family of all nations.",
   // "To accomplish No. 1 above, holiness will be our lifestyle.",
   // "To accomplish No. 2 and 3 above, we will plant churches within five minutes walking distance in every city and town of developing countries and within five minutes driving distance in every city and town of developed countries.We will pursue these objectives until every Nation in the world is reached for the Lord Jesus Christ.",
+  // const list = [
+  // "To make heaven.",
+  // "To take as many people with us.",
+  // "To have a member of RCCG in every family of all nations.",
+  // "To accomplish No. 1 above, holiness will be our lifestyle.",
+  // "To accomplish No. 2 and 3 above, we will plant churches within five minutes walking distance in every city and town of developing countries and within five minutes driving distance in every city and town of developed countries.We will pursue these objectives until every Nation in the world is reached for the Lord Jesus Christ.",
   //];
   return (
     <div className="">
@@ -55,9 +65,14 @@ export default function AboutUs() {
             arrows={false}
             autoPlaySpeed={3000}
             //   renderDotsOutside
+            //   renderDotsOutside
             rewindWithAnimation
           >
             {img.map((img, index) => (
+              <div
+                key={index}
+                className="historyBox flex justify-center items-center overflow-hidden w-full md:h-[500px] pt-16"
+              >
               <div
                 key={index}
                 className="historyBox flex justify-center items-center overflow-hidden w-full md:h-[500px] pt-16"
@@ -79,6 +94,7 @@ export default function AboutUs() {
           <AboutUsView />
         </div>
       </div>
+
       <div className="flex justify-center px-16 md:px-28 lg:px-28 flex-col gap-5 items-center">
         <div className="flex flex-col md:flex-row gap-3 w-full py-5 md:py-8 lg:py-12 justify-center items-center">
           <div className="w-full flex flex-col gap-3">
