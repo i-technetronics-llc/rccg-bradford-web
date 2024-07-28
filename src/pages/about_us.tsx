2b1364e958d324d27c4cbba550c8aebb18054795
Quadds <akinpelumoyo@gmail.com>
2024-07-28 12:58:01 +0100

    fuix

--- src/pages/about_us.tsx
+++ src/pages/about_us.tsx
@@ -12,7 +12,6 @@ export default function AboutUs() {
   const [isHovered, setIsHovered] = useState(false);
 
   const img = [
-  const img = [
     "https://www.rccgbradford.org.uk/wp-content/uploads/2019/05/rccg3-1.jpg",
     "https://www.rccgbradford.org.uk/wp-content/uploads/2019/05/rccg8.jpg",
     "https://www.rccgbradford.org.uk/wp-content/uploads/2019/05/rccg8.jpg",
@@ -73,17 +72,18 @@ export default function AboutUs() {
                 key={index}
                 className="historyBox flex justify-center items-center overflow-hidden w-full md:h-[500px] pt-16"
               >
-              <div
-                key={index}
-                className="historyBox flex justify-center items-center overflow-hidden w-full md:h-[500px] pt-16"
-              >
-                <Image
-                  src={img}
-                  alt="historyPix"
-                  width={500}
-                  height={500}
-                  className="w-full h-full object-cover"
-                />
+                <div
+                  key={index}
+                  className="historyBox flex justify-center items-center overflow-hidden w-full md:h-[500px] pt-16"
+                >
+                  <Image
+                    src={img}
+                    alt="historyPix"
+                    width={500}
+                    height={500}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
               </div>
             ))}
           </Carousel>
@@ -95,57 +95,6 @@ export default function AboutUs() {
         </div>
       </div>
 
-      <div className="flex justify-center px-16 md:px-28 lg:px-28 flex-col gap-5 items-center">
-        <div className="flex flex-col md:flex-row gap-3 w-full py-5 md:py-8 lg:py-12 justify-center items-center">
-          <div className="w-full flex flex-col gap-3">
-            <div className="flex flex-col gap-3 my-5">
-              <p className="text-xl md:text-3xl lg:text-4xl">OUR MISSION</p>
-              <div className="h-[3px] w-[10%] bg-gradient-to-r from-primary to-secondary"></div>
-            </div>
-            <p className="text-sm tracking-wider">
-              Our vision is exactly the same as those of The Redeemed Christian
-              Church of God. They are as follows:
-            </p>
-            {list.map((item, index) => (
-              <div
-                className="flex gap-2 text-[#333333] items-center"
-                key={index}
-              >
-                <LuCheckCheck className="w-[10%]" />
-                <p className="text-sm w-[90%]">{item}</p>
-              </div>
-            ))}
-          </div>
-          <div className="w-full">
-            <div className="flex flex-col gap-3 my-5">
-              <p className="text-xl md:text-3xl lg:text-4xl text-center">
-                COME AS YOU ARE
-              </p>
-            </div>
-            <div
-              className="w-full h-[300px] overflow-hidden"
-              style={{
-                borderRadius: isHovered
-                  ? "49% 51% 53% 47% / 47% 48% 52% 53%"
-                  : "64% 36% 78% 22% / 16% 76% 24% 84%",
-                transition: "ease-in-out 1s",
-              }}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <Image
-                src={
-                  "https://www.rccgbradford.org.uk/wp-content/uploads/2016/01/men.gif"
-                }
-                alt="historyPix"
-                width={500}
-                height={500}
-                className="w-full h-full"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
       <Footer />
     </div>
   );
