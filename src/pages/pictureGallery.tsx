a917265f6f50fa30c01f7b551acb00e5ff560ddc
Quadds <akinpelumoyo@gmail.com>
2024-08-04 19:52:08 +0100

    fix

--- src/pages/pictureGallery.tsx
+++ src/pages/pictureGallery.tsx
@@ -58,7 +58,7 @@ export default function PictureGallery() {
   return (
     <div className="">
       <Header />
-      <div className="w-full flex justify-center">
+      <div className="w-full mt-[80px] lg:mt-[120px] flex justify-center">
         <div className="w-[90%]">
           <div className="px-8 py-4 flex flex-col gap-2">
             <div className="px-8 py-4 flex flex-col gap-2">
