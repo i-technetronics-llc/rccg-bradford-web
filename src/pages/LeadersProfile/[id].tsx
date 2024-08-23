c9319226481a9baa2b07492ed60ca3d026b6dce5
Quadds <akinpelumoyo@gmail.com>
2024-08-23 13:41:10 +0100

    made some changes in id.tsx file

--- src/pages/LeadersProfile/[id].tsx
+++ src/pages/LeadersProfile/[id].tsx
@@ -23,22 +23,23 @@ export default function LeaderProfile() {
 
 
   return (
-    <div className="min-h-screen">
-      <Header />
-      <div className="flex flex-col md:flex-row items-center p-4 md:p-8 lg:p-12">
-        <img
-          src={leader.img}
-          alt={leader.name}
-          className="w-full md:w-1/3 rounded-lg shadow-lg pt-20"
-        />
-        <div className="md:ml-8 mt-4 md:mt-0">
-          <h1 className="text-2xl md:text-4xl font-bold">{leader.name}</h1>
-          <p className="text-lg md:text-xl">{leader.role}</p>
-          <p className="mt-4">{leader.desc}</p>
-        </div>
+  <div className="min-h-screen flex flex-col">
+    <Header />
+    <div className="flex-grow flex flex-col md:flex-row items-center p-4 md:p-8 lg:p-12">
+      <img
+        src={leader.img}
+        alt={leader.name}
+        className="w-full md:w-1/3 rounded-lg shadow-lg pt-20"
+      />
+      <div className="md:ml-8 mt-4 md:mt-0">
+        <h1 className="text-2xl md:text-4xl font-bold">{leader.name}</h1>
+        <p className="text-lg md:text-xl">{leader.role}</p>
+        <p className="mt-4">{leader.desc}</p>
       </div>
-        <Footer />
     </div>
-  );
+    <Footer />
+  </div>
+);
+
   
 }
