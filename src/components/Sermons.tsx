9c7897b84949a3b4d0ae7b8ee1ab4840c8cdf24c
StephenAdeosun <stephenadeosun411@gmail.com>
2025-03-04 22:28:02 +0100

    SERMON

--- src/components/Sermons.tsx
+++ src/components/Sermons.tsx
@@ -12,7 +12,7 @@ export default function Sermons({ latestSermons }: LatestSermonProps) {
     <div className="flex items-center justify-center">
       <div className="my-8 md:my-20 flex flex-col w-[80%] items-center justify-center gap-8 px-8">
         <p className="text-xl md:text-2xl font-semibold lg:text-3xl">
-          Latest Sermons
+        Open Heavens Daily Devotional
         </p>
         <div className="grid grid-cols-1 gap-5 w-full md:grid-cols-2">
           {latestSermons.map((item, index) => (
@@ -29,7 +29,7 @@ export default function Sermons({ latestSermons }: LatestSermonProps) {
           className="bg-primary rounded-full px-5 py-2 text-white font-semibold"
           onClick={() => router.push("/videoGallery")}
         >
-          ALL SERMONS
+          READ MORE
         </button>
       </div>
     </div>
