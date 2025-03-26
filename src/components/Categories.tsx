a0f61b8ccb5351193b12ace734cbb644e5efe254
StephenAdeosun <stephenadeosun411@gmail.com>
2025-03-26 09:57:16 +0100

    new

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -49,7 +49,7 @@ export default function Categories({ categories }: CategoryProps) {
     <>
  
  <section className="bg-gray-100 py-12 px-6">
-  <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-[1fr_1px_1fr] gap-8 items-center text-center md:text-left">
+  <div className="max-w-8xl mx-auto grid grid-cols-1 md:grid-cols-[1fr_1px_1fr] gap-8 items-center text-center md:text-left">
     
     {/* Share Your Testimony */}
     <div className="p-6 rounded-lg">
@@ -58,11 +58,15 @@ export default function Categories({ categories }: CategoryProps) {
         Has God done something amazing in your life? We'd love to hear your story!
         Share your testimony to encourage and bless others.
       </p>
-      <Link href="/contact"> <button className="mt-4 bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700">
+      {/* <Link href="/contact"> <button className="mt-4 bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700">
         GIVE A TESTIMONY
       </button>
-      </Link>
-     
+      </Link> */}
+      <Link href='/contact' target="_blank">
+              <button className="bg-primary rounded-full mt-4 px-5 py-2 text-white font-semibold hover:bg-primary-dark transition">
+               GIVE A TESTIMONY
+              </button>
+            </Link>
     </div>
 
     {/* Divider (Hidden on Small Screens) */}
@@ -70,16 +74,21 @@ export default function Categories({ categories }: CategoryProps) {
 
     {/* Join Our Ministry */}
     <div className="p-6 rounded-lg">
-      <h2 className="text-2xl font-bold text-gray-800">Join Our Ministry!</h2>
+      <h2 className="text-2xl font-bold text-gray-800">Join Our Team!</h2>
       <p className="text-gray-600 mt-2">
         We’re hiring a Minister of Religion to lead worship, provide pastoral care,
         and foster spiritual growth in our vibrant Pentecostal church in Bradford.
-        Apply by <strong>25/04/2025!</strong> Kindly click the button to apply.
+        Apply by <strong><em>25/04/2025!</em></strong> Kindly click the button to apply.
       </p>
-      <Link  href="https://g.co/kgs/Jba99Nz"> <button className="mt-4 bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700">
+      {/* <Link  href="https://g.co/kgs/Jba99Nz"> <button className="mt-4 bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700">
         APPLY NOW
       </button>
-      </Link>
+      </Link> */}
+      <Link href='https://g.co/kgs/Jba99Nz' target="_blank">
+              <button className="bg-primary mt-4 rounded-full px-5 py-2 text-white font-semibold hover:bg-primary-dark transition">
+              APPLY NOW
+              </button>
+            </Link>
      
     </div>
 
